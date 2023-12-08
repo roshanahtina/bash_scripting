f83041e341e5616b8fc697ed1338de79efd7eb94
roshanahtina <roshan.ahtina@gmail.com>
2023-12-08 14:40:37 +0000

    This is the backup script

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+LOG_TARGET="/home/$USER/LOG_BACKUP"
+LOG_SOURCE="/usr/bin/"
+LOG_FILE="/home/$USER/LOG_BACKUP_FILE.txt"
+
+echo "Copy the file from $LOG_SOURCE to $LOG_TARGET"
+mkdir $LOG_TARGET
+cp -v $LOG_SOURCE $LOG_TARGET >> $LOG_FILE 2>&1
+echo "Coyping the file from $LOG_SOURCE to $LOG_TARGET is finished"
+exit" 
