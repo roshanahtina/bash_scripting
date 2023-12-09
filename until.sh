07577d3a8d83cd5275bd4d4ae6891a4ff890c206
Roshan Munavar <roshan.ahtina@gmail.com>
2023-12-09 23:49:51 +0530

    Update until.sh

--- until.sh
+++ until.sh
@@ -4,10 +4,13 @@ echo "Displaying the COUNTDOWN from 1..10"
 
 until [ $COUNTER -lt 10 ]; do
 	echo "The COUNTER is $COUNTER"
-	#let COUNTER=COUNTER+1
+	#let COUNTER-=COUNTER
 	((COUNTER--))
 done
 
 exit
+
+
+
 
 
