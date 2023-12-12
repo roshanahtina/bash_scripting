f3c0dd2a7e6c40bf46002aa20e9871ded4c23926
Roshan Munavar <roshan.ahtina@gmail.com>
2023-12-12 19:38:13 +0530

    Removed extra taps and spaces regex.sh

--- regex.sh
+++ regex.sh
@@ -6,18 +6,18 @@ fi
 MYLOG=$1
 function ctrlc {
 	        rm -rf /home/$USER/work_backup
-		        rm -f /home/$USER/$MYLOG
-			        echo "Received Ctrl+C"
-				        exit 255
-				}
-				trap ctrlc SIGINT
-				echo "Timestamp before work is done $(date +"%D %T")" >> /home/$USER/$MYLOG
-				echo "Creating backup directory" >> /home/$USER/$MYLOG
-				mkdir /home/$USER/work_backup
-				echo "Copying Files" >> /home/$USER/$MYLOG
-				for i in $(ls work | grep -E 'adent|financ'); do
-					        cp -v /home/$USER/work/$i /home/$USER/work_backup >> /home/$USER/$MYLOG
-					done
-					#cp -v /home/$USER/work/* /home/$USER/work_backup/ >> /home/$USER/$MYLOG
-					echo "Finished Copying Files" >> /home/$USER/$MYLOG
-					echo "Timestamp after work is done $(date +"%D %T")" >> /home/$USER/$MYLOG 
+		rm -f /home/$USER/$MYLOG
+		echo "Received Ctrl+C"
+		exit 255
+}
+trap ctrlc SIGINT
+echo "Timestamp before work is done $(date +"%D %T")" >> /home/$USER/$MYLOG
+echo "Creating backup directory" >> /home/$USER/$MYLOG
+mkdir /home/$USER/work_backup
+echo "Copying Files" >> /home/$USER/$MYLOG
+for i in $(ls work | grep -E 'adent|financ'); do
+	cp -v /home/$USER/work/$i /home/$USER/work_backup >> /home/$USER/$MYLOG
+done
+#cp -v /home/$USER/work/* /home/$USER/work_backup/ >> /home/$USER/$MYLOG
+echo "Finished Copying Files" >> /home/$USER/$MYLOG
+echo "Timestamp after work is done $(date +"%D %T")" >> /home/$USER/$MYLOG
