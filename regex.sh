e45b9e7fea3a0a3443dec0125937ee4c18047a05
roshanahtina <roshan.ahtina@gmail.com>
2023-12-12 14:05:21 +0000

    Added a regex file

---
+++ regex.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+	        echo "You have failed to pass a parameter. Please try again."
+		        exit 255;
+fi
+MYLOG=$1
+function ctrlc {
+	        rm -rf /home/$USER/work_backup
+		        rm -f /home/$USER/$MYLOG
+			        echo "Received Ctrl+C"
+				        exit 255
+				}
+				trap ctrlc SIGINT
+				echo "Timestamp before work is done $(date +"%D %T")" >> /home/$USER/$MYLOG
+				echo "Creating backup directory" >> /home/$USER/$MYLOG
+				mkdir /home/$USER/work_backup
+				echo "Copying Files" >> /home/$USER/$MYLOG
+				for i in $(ls work | grep -E 'adent|financ'); do
+					        cp -v /home/$USER/work/$i /home/$USER/work_backup >> /home/$USER/$MYLOG
+					done
+					#cp -v /home/$USER/work/* /home/$USER/work_backup/ >> /home/$USER/$MYLOG
+					echo "Finished Copying Files" >> /home/$USER/$MYLOG
+					echo "Timestamp after work is done $(date +"%D %T")" >> /home/$USER/$MYLOG 
